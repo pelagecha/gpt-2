7ae81a3024e22fba74fd935ac05cac8884af18c1
amency <nikitapelagecha@gmail.com>
2024-10-02 16:57:16 +0100

    successfully loaded pretrained weights

--- train.py
+++ train.py
@@ -9,26 +9,6 @@ import transformers
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -48,7 +28,7 @@ class CausalSelfAttention(nn.Module):
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, num_heads, T, head_size)
 
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.bias[:,:,:T,:T]) == 0, float("-inf")
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float("-inf"))
         att = F.softmax(att, dim=-1)
 
         y = (att @ v).transpose(1, 2).contiguous().view(B,T,C)
@@ -179,9 +159,33 @@ class GPT(nn.Module):
 
     
 
+# =========== Generation ===========
 num_return_sequences = 5
 max_length = 30
 
 model = GPT.from_pretrained("gpt2")
 model.eval()
-model.to("mps")
+model.to("cuda")
+
+import tiktoken
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to("cuda")
+
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits, _ = model(x)
+        logits = logits[:, -1, :]
+        probs  = F.softmax(logits, dim=-1)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix     = torch.multinomial(topk_probs, 1)
+        xcol   = torch.gather(topk_indices, -1, ix)
+        x      = torch.cat((x, xcol), dim=1)
+
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
